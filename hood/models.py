740417a40c11d4e1ea31db3aa0e5dd8b543d962b
yontey <muasajohn01@gmail.com>
2019-05-30 11:05:46 +0300

    Created the necessary models for the application

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=20)
+    location = models.CharField(max_length=30)
+    count = models.IntegerField(default=0)
+    
+    
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=200)
+    neighborhood = models.ForeignKey(Neighbourhood,null=True,blank=True) 
+    
+class Business(models.Model):
+    
+    
+
+

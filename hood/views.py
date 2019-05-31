e9c3544ae3602089fc0fe588bb6a232698c31793
yontey <muasajohn01@gmail.com>
2019-05-31 17:21:36 +0300

    created model forms

--- hood/views.py
+++ hood/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from .models import *
+from django.http import Http404
  
 
 # Create your views here.
@@ -8,3 +9,15 @@ def index(request):
     hoods = Neighbourhood.objects.all()
     
     return render(request,'index.html',{"hoods":hoods})
+
+def single_hood(request,hood_id):
+   try:
+       hoods = Neighbourhood.objects.get(id=hood_id) 
+       businesses = Business.objects.filter(id=hood_id)
+    
+    except Exception as e:
+        raise Http404()
+    
+    
+    
+    return render(request,'hood.html',)

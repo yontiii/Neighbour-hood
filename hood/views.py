5323cca9d2283194385f8fc2d94cdcd97aaaf61c
yontey <muasajohn01@gmail.com>
2019-06-05 16:25:13 +0300

    Created profile view function

--- hood/views.py
+++ hood/views.py
@@ -56,4 +56,15 @@ def single_hood(request,location):
 
 
 
+@login_required(login_url='/accounts/login/')
+def profile(request,username):
+    profile = User.objects.get(username=username)
+    
+    try:
+        profile_details = Profile.get_by_id(profile.id)
+    except:
+        profile_details = Profile.filter_by_id(profile.id)
+    projects = Projects.get_profile_projects(profile.id)
+    
+    return render(request, 'users/profile.html',{"profile":profile,"profile_details":profile_details,"projects":projects}) 
     
